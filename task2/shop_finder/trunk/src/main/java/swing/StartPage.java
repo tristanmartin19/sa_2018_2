b673a3dcd5ed7f75d79e408912c3599900137066
alex2studiert <alex2studiert@b64268a6-6601-0010-afbb-afac277447bb>
2018-11-02 22:24:51 +0000

    Connection to Database

--- task2/shop_finder/trunk/src/main/java/swing/StartPage.java
+++ task2/shop_finder/trunk/src/main/java/swing/StartPage.java
@@ -1,6 +1,13 @@
 package swing;
 
+import toolbox.datahandler;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class StartPage {
     private JTable tabResult;
@@ -8,12 +15,46 @@ public class StartPage {
     private JButton btnTestCon;
 
 
+    public StartPage() {
+        btnTestCon.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+
+                datahandler dh = new datahandler();
+                try {
+                    Connection connection = dh.connectToDB();
+
+                    if (connection != null) {
+
+                    }
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+
+                }
+
+            }
+        });
+    }
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("StartPage");
         frame.setContentPane(new StartPage().PanelMain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        String Colums[] = {"name", "ID"};
+
+        String data[][] = {{"alex", "1"},{"Daniel", "2"}};
+
+        JTable table = new JTable(data, Colums);
+        table.setPreferredScrollableViewportSize( new Dimension(500, 50));
+        //table.setFillsViewportHeight(true);
+
+        JScrollPane scrollPane = new JScrollPane(table);
     }
 
 }
