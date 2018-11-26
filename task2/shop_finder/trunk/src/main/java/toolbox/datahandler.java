df11d41cab6ac229560deac491c1fb25a32f8355
soundsliketristn <soundsliketristn@b64268a6-6601-0010-afbb-afac277447bb>
2018-11-26 16:40:18 +0000

    implemented search implemented getAllShops implemented EditShop implemented DeleteShop -----Important: password for DB changed-----

--- task2/shop_finder/trunk/src/main/java/toolbox/datahandler.java
+++ task2/shop_finder/trunk/src/main/java/toolbox/datahandler.java
@@ -4,14 +4,14 @@ import java.sql.*;
 
 public class datahandler {
 
-    public enum orderBy { shop_id, name, distance };
+    public enum orderBy { shop_id, name, distance }
     private calculator Calc = new calculator();
 
 
     public Connection connectToDB () throws SQLException, ClassNotFoundException {
         String Username = "root";
-        String Pwd = "root";
-        String URL = "jdbc:mysql://localhost/sa_shop_finder ";
+        String Pwd = "password";
+        String URL = "jdbc:mysql://localhost/sa_shop_finder";
         String BugfixURL = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&" +
                 "useLegacyDatetimeCode=false&serverTimezone=UTC";
         Class.forName("com.mysql.jdbc.Driver");
@@ -24,9 +24,10 @@ public class datahandler {
     public ResultSet getAllShops(Connection connection) throws SQLException{
 
         Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT * from shops order by name  ");
+        ResultSet resultSet = statement.executeQuery("SELECT * from shops " +
+                "INNER JOIN shop_has_categories category on shops.shop_id = category.shop_id " +
+                "INNER join shop_categories sc on category.category_id = sc.category_id ");
 
-        statement.close();
 
         return resultSet;
     }
@@ -43,6 +44,8 @@ public class datahandler {
 
         String SQLCalcDistance = "";
 
+        String sql_category_part = "";
+
         if (distance != 0){
 
             while (resultSetPOI.next()) {
@@ -55,12 +58,15 @@ public class datahandler {
 
         }
 
+
+
+
         if ( categoryID != 0 && poiID != 0){
             // Get shops with name and cat arroind a POI
             // Calculate the Range arround the POI
 
             // Get the Distnce for each shop and compare it with the max dist to the POI in the Query
-            resultSet = statement.executeQuery("SELECT shops.shop_id, shops.name, homepage, longitude, latitude, " +
+            resultSet = statement.executeQuery("SELECT *,  " +
                     "" + SQLCalcDistance + " as distance  from shops " +
                     "INNER JOIN shop_has_categories category on shops.shop_id = category.shop_id " +
                     "INNER join shop_categories sc on category.category_id = sc.category_id " +
@@ -70,7 +76,7 @@ public class datahandler {
 
         }
         else if (shopName != "" &&  poiID != 0  ){
-            resultSet = statement.executeQuery("SELECT shops.shop_id, shops.name, homepage, longitude, latitude ," +
+            resultSet = statement.executeQuery("SELECT *," +
                     "" + SQLCalcDistance + " as distance from shops " +
                     "INNER JOIN shop_has_categories category on shops.shop_id = category.shop_id " +
                     "INNER join shop_categories sc on category.category_id = sc.category_id " +
@@ -85,21 +91,21 @@ public class datahandler {
             // Get the Distnce for each shop and compare it with the max dist to the POI in the Query
 
             orderBy = datahandler.orderBy.distance;
-            resultSet = statement.executeQuery("SELECT shops.shop_id, shops.name, homepage, longitude, latitude, " + SQLCalcDistance + " as distance from shops " +
+            resultSet = statement.executeQuery("SELECT *, " + SQLCalcDistance + " as distance from shops " +
                     "INNER JOIN shop_has_categories category on shops.shop_id = category.shop_id " +
                     "INNER join shop_categories sc on category.category_id = sc.category_id " +
                     "where " + SQLCalcDistance + " <= "  + distance +
                     " order by " + orderBy.toString() );
         }
         else if (shopName != "" && categoryID != 0 ){
-            resultSet = statement.executeQuery("SELECT shops.shop_id, shops.name, homepage, longitude, latitude  from shops " +
+            resultSet = statement.executeQuery("SELECT *  from shops " +
                     "INNER JOIN shop_has_categories category on shops.shop_id = category.shop_id " +
                     "INNER join shop_categories sc on category.category_id = sc.category_id " +
                     "where shops.name like '%" + shopName + "%' and sc.category_id =" + categoryID + " " +
                     "order by " + orderBy.toString() );
         }
         else {
-            resultSet = statement.executeQuery("SELECT shops.shop_id, shops.name, homepage, longitude, latitude  from shops " +
+            resultSet = statement.executeQuery("SELECT *  from shops " +
                     "INNER JOIN shop_has_categories category on shops.shop_id = category.shop_id " +
                     "INNER join shop_categories sc on category.category_id = sc.category_id " +
                     "where shops.name like '%" + shopName + "%' or sc.category_id =" + categoryID + " " +
@@ -116,6 +122,23 @@ public class datahandler {
 
         return resultSet;
 
+    }
+
+    public ResultSet getCategory(Connection connection, String name) throws SQLException{
+
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * from shop_categories WHERE name_category = '"+name+"'");
+
+        return resultSet;
+
+    }
+
+    public ResultSet getPointOfInterest(Connection connection, String name) throws SQLException{
+
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * from points_of_interest WHERE name ='"+name+"'");
+
+        return resultSet;
 
     }
 
@@ -162,11 +185,11 @@ public class datahandler {
                            double latitude, String homepage ) throws SQLException{
         // Todo: Test when UI is ready
         Statement statement = connection.createStatement();
+
         statement.executeQuery("UPDATE shops " +
-                "SET osm_id = " + osm_id + " , longitude = " + longitude + ", latitude = " + latitude + " , " +
+                "SET longitude = " + longitude + ", latitude = " + latitude + " , " +
                 "name = ' " + name + "  ' , homepage = ' " + homepage + " ' " +
                 "WHERE shop_id = " + shopID );
-
 
     }
 
@@ -193,31 +216,39 @@ public class datahandler {
         ResultSet rsCategories;
         ResultSet rsCategory;
         // Search for Connected Categories
-        rsCategories = statement.executeQuery(" SELECT id,  category_id from shop_has_categories " +
+        rsCategories = statement.executeQuery(" SELECT id,  category_id, shop_id from shop_has_categories " +
                 "where shop_id = " + shopID );
+
         // Check if this is the last shop of the Connected categories and delete them if it is so
         while (rsCategories.next()) {
+            int category_id = rsCategories.getInt("category_id");
+            int shop_id = rsCategories.getInt("shop_id");
+
             rsCategory = statement.executeQuery("select id from shop_has_categories " +
-                    "where category_id = " + rsCategories.getInt("category_id"));
+                    "where category_id = " + category_id);
 
             while (rsCategory.next()){
                 if (rsCategory.isFirst() && rsCategory.isLast()){
                     // No more shops left -> delete this Cat
                     statement.execute("delete from shop_categories " +
-                            "where category_id = " + rsCategories.getInt("category_id") );
+                            "where category_id = " + category_id );
                 }
                 else
                     continue;
             }
 
+
+
             // Done searching for related cats, delete this Row
             statement.execute("delete from shop_has_categories " +
-                    "where category_id = " + rsCategories.getInt("category_id") );
+                    "where shop_id = " + shop_id );
 
         }
 
         statement.execute("delete from shops " +
                 "where shop_id = " + shopID );
+
+
 
 
     }
--- task2/shop_finder/trunk/src/main/java/toolbox/shop.java
+++ task2/shop_finder/trunk/src/main/java/toolbox/shop.java
@@ -1,20 +1,50 @@
 package toolbox;
 
-public class shop {
-    String name;
-    double longitude;
-    double latitude;
-    String category;
-    String homepage;
-    int id;
+import java.sql.Connection;
+import java.sql.SQLException;
 
-    public shop(String name, double longitude, double latitude, String category, String homepage, int id) {
+public class shop {
+
+    private String name;
+    private double longitude;
+    private double latitude;
+    private String category;
+    private String homepage;
+    private int id;
+    private datahandler data_handler;
+
+    public shop(String name, double longitude, double latitude, String category, String homepage, int id, datahandler data_handler_) {
         this.name = name;
         this.longitude = longitude;
         this.latitude = latitude;
         this.category = category;
         this.homepage = homepage;
         this.id = id;
+        this.data_handler = data_handler_;
+    }
+
+    public void editShop(String new_name, double new_longitude, double new_latitude, String new_homepage) {
+        this.name = new_name;
+        this.longitude = new_longitude;
+        this.latitude = new_latitude;
+        this.homepage = new_homepage;
+
+        try {
+            Connection connection = data_handler.connectToDB();
+            data_handler.EditShop(connection, id,name, 0, longitude, latitude, homepage);
+        }
+        catch (SQLException ex1) {}
+        catch (ClassNotFoundException ex2) {}
+    }
+
+    public void deleteShop() {
+        try {
+            Connection connection = data_handler.connectToDB();
+            data_handler.DeleteShop(connection, id);
+        }
+        catch (SQLException ex1) {}
+        catch (ClassNotFoundException ex2) {}
+
     }
 
     public String getName() {
